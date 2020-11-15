9902a38ba7c856374b0745b03b2b506bc14706cb
Feynom <chreptak12@gmail.com>
2020-11-15 21:28:52 +0200

    1

--- lab6.3/lab6.3.cpp
+++ lab6.3/lab6.3.cpp
@@ -4,7 +4,12 @@
 
 using namespace std;
 
+void Create_i(int* a, const int SIZE);
+void Print_i(int* a, const int SIZE);
 void sort_i(int a[], const int SIZE);
+
+void Create_r(int* a, const int SIZE, int i);
+void Print_r(int* a, const int SIZE, int i);
 void sort_r(int a[], const int SIZE, int i, int j);
 
 int main()
@@ -18,24 +23,38 @@ int main()
     cout << "Введіть розмір масиву a[]:"; cin >> SIZE;
     int* a = new int[SIZE];
 
-    cout << "a[] = ";
-    for (int i = 0; i < SIZE; i++)
-    {
-        a[i] = rand() % SIZE;
-        cout << a[i] << " ";
-    }
     cout << "\nІтераційний спосіб:" << endl;
+    cout << "Масив: ";
+    Create_i(a, SIZE);
+    Print_i(a, SIZE); cout << endl;
     cout << "\nВпорядкований масив за спаданням:";
     sort_i(a, SIZE);
+    Print_i(a, SIZE); cout << endl;
+
     cout << "\nРекурсивний спосіб:" << endl;
+    cout << "Масив: ";
+    Create_r(a, SIZE, 0);
+    Print_r(a, SIZE, 0); cout << endl;
     cout << "\nВпорядкований масив за спаданням:";
-    sort_r(a, SIZE, 1, 0);
+    sort_r(a, SIZE, 0, 0);
+    
+
     for (int j = 0; j < SIZE; j++)
         cout << a[j] << " ";
     delete[]a;
     return 0;
 }
 //ітераційний спосіб
+void Create_i(int* a, const int SIZE)
+{
+    for (int i = 0; i < SIZE; i++)
+        a[i] = rand() % SIZE;
+}
+void Print_i(int* a, const int SIZE)
+{
+    for (int i = 0; i < SIZE; i++)
+        cout << a[i] << " ";
+}
 void sort_i(int a[], const int SIZE)
 {
     for(int i = 1; i < SIZE; i++)
@@ -46,21 +65,36 @@ void sort_i(int a[], const int SIZE)
                 a[j] = a[j + 1];
                 a[j + 1] = temp;
             }
-    for (int j = 0; j < SIZE; j++)
-        cout << a[j] << " ";
-
 }
 //рекурсивний спосіб
+void Create_r(int* a, const int SIZE, int i)
+{
+    if (i < SIZE)
+    {
+        a[i] = rand() % SIZE;
+        return Create_r(a, SIZE, i + 1);
+    }
+}
+void Print_r(int* a, const int SIZE, int i)
+{
+    if (i < SIZE)
+    {
+        cout << a[i] << " ";
+        return Print_r(a, SIZE, i + 1);
+    }
+}
 void sort_r(int a[], const int SIZE, int i, int j)
 {
-    if(a[j] < a[j + 1])
+    if (j < SIZE - 1)
     {
-        int temp = a[j];
-        a[j] = a[j + 1];
-        a[j + 1] = temp;
+        if (a[j] < a[j + 1])
+        {
+            int temp = a[j];
+            a[j] = a[j + 1];
+            a[j + 1] = temp;
+        }
+        return sort_r(a, SIZE, i, j + 1);
     }
-    if (j > SIZE - i - 1)
-        sort_r(a, SIZE, i, j + 1);
-    if (j > SIZE - 1)
-        sort_r(a, SIZE, i + 1, 0);
+    if (i < SIZE - 1)
+        return sort_r(a, SIZE, i + 1, 0);
 }
